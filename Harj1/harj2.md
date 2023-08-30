b01f9b77e7ceb5c36855b855de8737640259014a
Petri Peltomaa <AC7995@student.labranet.jamk.fi>
2023-08-30 13:37:35 +0300

    Lisätty Harjoitus 1 ja Kuvat kansio sekä tehtävät 1-3

---
+++ Harj1/harj2.md
@@ -0,0 +1,4 @@
+Alla kuvat jsbin ja jsfiddle sivustoilta<br/>
+
+![](/Kuvat/harj2_jsbin.png)<br/>
+![](/Kuvat/harj2_jsfiddle.png)<br/>
---
+++ Harj1/harj3.js
@@ -0,0 +1,5 @@
+let opiskelu = ["JavaScriptin", "opiskelu", "on", "ihanaa"];
+
+for (let value of opiskelu) {
+    console.log(opiskelu.indexOf(value) + " " + value);
+  }
