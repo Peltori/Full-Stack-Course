cf71f095490cda732c44385bb9c9532cd16b1d9b
Petri Peltomaa <AC7995@student.labranet.jamk.fi>
2023-09-20 19:17:29 +0300

    Lisätty Harj4 teht1 sekä .css ja .js tiedostot

---
+++ Data/housescript.js
@@ -0,0 +1,68 @@
+async function fetchHouses() {
+    let url = "/Data/talotiedot.json";
+    try {
+        let result = await fetch(url);
+        return await result.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function renderHouses() {
+    let houses = await fetchHouses();
+    
+    // check console that json data is loaded correctly
+    console.log(houses);
+    
+    // find empty houses div
+    let housediv = document.getElementById("houses");
+    
+    // loop through your JSON data
+    houses.forEach(house => {
+        
+        // create a new house container  
+        let housecontainer = document.createElement("div");
+        housecontainer.className = "houseContainer";
+        
+        // create an image
+        let image = document.createElement("img");
+        image.src = house.image;
+        image.className = "houseImage";
+        
+        // create p element as header
+        let header = document.createElement("p");
+        header.className = "header";
+        let headerText = document.createTextNode(house.address);
+        header.appendChild(headerText);
+        
+        // create p element as houseSize
+        let houseSize = document.createElement("p");
+        houseSize.className = "houseSize";
+        let houseSizeText = document.createTextNode(house.size);
+        houseSize.appendChild(houseSizeText);
+        
+        // create p element as description
+        let description = document.createElement("p");
+        description.className = "description";
+        let descriptionText = document.createTextNode(house.text);
+        description.appendChild(descriptionText);
+        
+        // create p element as price
+        let price = document.createElement("p");
+        price.className = "price";
+        let priceText = document.createTextNode(house.price);
+        price.appendChild(priceText);
+        
+        // add created elements to container
+        housecontainer.appendChild(image);
+        housecontainer.appendChild(header);
+        housecontainer.appendChild(houseSize);
+        housecontainer.appendChild(description);
+        housecontainer.appendChild(price);
+        
+        // add house to div
+        housediv.appendChild(housecontainer); 
+    });
+  }
+
+  renderHouses();
