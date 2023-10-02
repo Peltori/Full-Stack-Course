89741373cc769b129149cfa0a61eb2b88cd201c5
Petri Peltomaa <AC7995@student.labranet.jamk.fi>
2023-10-02 20:10:29 +0300

    Muokkausta koodiin

--- Data/namesearch.js
+++ Data/namesearch.js
@@ -1,3 +1,8 @@
+let isArrowKeyPressed = false; // Track whether an arrow key is currently pressed
+let selectedNameIndex = -1; // Define selectedNameIndex globally
+let namediv; // Define namediv globally
+
+
 async function fetchNames() {
     let url = "/Data/names.json";
     try {
@@ -11,6 +16,11 @@ async function fetchNames() {
 function init(){
     let nameInput = document.getElementById("nameInput");
     nameInput.addEventListener("keyup", renderNames);
+    nameInput.addEventListener("keydown", handleKeyDown);
+    nameInput.addEventListener("keyup", handleKeyUp);
+    namediv = document.getElementById("nameSpace"); // Initialize namediv
+
+    //let selectedNameIndex = -1;
 }
 
 async function renderNames() {
@@ -21,13 +31,8 @@ async function renderNames() {
     
     let searchText = document.getElementById("nameInput").value.toLowerCase();
 
-    // find empty div
-    let namediv = document.getElementById("nameSpace");
-
     // remove names from div element before adding new one
-    while (namediv.firstChild) {
-        namediv.removeChild(namediv.firstChild);
-    }
+    namediv.textContent = "";
 
     if (searchText === "") {
         return; // Do not show any results if input empty
@@ -37,41 +42,58 @@ async function renderNames() {
     names.forEach(name => {
         let lowerCaseName = name.toLowerCase();
         if (lowerCaseName.startsWith(searchText)) {
-            // create p element as person
-            let persons = document.createElement("p");
-            persons.className = "person";
-            let personsText = document.createTextNode(name);
-            persons.appendChild(personsText);
-            
-            // add created elements to div
-            namediv.appendChild(persons);
+            // Check if the name is an exact match with the search
+            if (lowerCaseName !== searchText) {
+                // create p element as person
+                let persons = document.createElement("p");
+                persons.className = "person";
+                let personsText = document.createTextNode(name);
+                persons.appendChild(personsText);
+                
+                // add created elements to div
+                namediv.appendChild(persons);
+            }
         }
     });
-  }
 
-function handleKeyPress(event) {
+    // Clear the namespace if the full name is in the search field
+    if (names.includes(searchText)) {
+        namediv.textContent = "";
+    }
+}
+
+  function handleKeyDown(event) {
     if (event.key === "ArrowUp") {
         // Up arrow: Select previous name
-        if (selectedNameIndex > 0) {
+        if (!isArrowKeyPressed && selectedNameIndex > 0) {
             selectedNameIndex--;
-            highlightSelectedName();
+            isArrowKeyPressed = true;
         }
     } else if (event.key === "ArrowDown") {
         // Down arrowkey: Select next name
-        if (selectedNameIndex < document.querySelectorAll(".person").length -1) {
+        if (!isArrowKeyPressed && selectedNameIndex < document.querySelectorAll(".person").length - 1) {
             selectedNameIndex++;
-            highlightSelectedName();
+            isArrowKeyPressed = true;
         }
     } else if (event.key === "Enter") {
-        // Enter: Select current name and place it to the input field
+        // Enter: Select current name and place it in the input field
         let selectedNameElement = document.querySelector(".person.selected");
         if (selectedNameElement) {
-            document.getElementById("nameInput").value = selectedNameElement.textContent;
+            selectedName = selectedNameElement.textContent;
+            document.getElementById("nameInput").value = selectedName;
             document.getElementById("nameSpace").textContent = ""; // Empty text content
+
+            event.preventDefault(); // Prevent the default behavior (form submission)
         }
     }
 
-    highlightSelectedName();
+    highlightSelectedName(); // Highlight based on selectedNameIndex
+}
+
+function handleKeyUp(event) {
+    if (event.key === "ArrowUp" || event.key === "ArrowDown") {
+        isArrowKeyPressed = false; // Reset the arrow key press flag when released
+    }
 }
 
 function highlightSelectedName() {
@@ -84,5 +106,3 @@ function highlightSelectedName() {
         }
     });
 }
-
-let selectedNameIndex = -1;
